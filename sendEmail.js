f2306514a35e9b5d6e0366337503d9e5f762447d
manojpannala <manojpannala@outlook.com>
2021-05-26 10:26:33 +0300

    minor fixes

--- sendEmail.js
+++ sendEmail.js
@@ -1,60 +1,57 @@
 const AWS = require('aws-sdk');
+
 const ses = new AWS.SES();
 
 exports.handler = async event => {
+    const { to, from, subject, text } = JSON.parse(event.body);
 
-    const { to, from, subject, text } = JSON.parse (event.body);
-
-    if ( !to || !from || !subject || !text ) {
-        return _400 ({message: 'missing parameter on request body'});
+    if (!to || !from || !subject || !text) {
+        return _400({ message: 'missing parameter on request body' });
     }
 
     const emailParams = {
         Destination: {
-            ToAddress: [to]
+            ToAddresses: [to],
         },
         Message: {
-            Body:{
-                Text: { Data: text }
+            Body: {
+                Text: { Data: text },
             },
-            Subject: { Data: subject }
+            Subject: { Data: subject },
         },
-        Source: from
-    }
+        Source: from,
+    };
 
     try {
         await ses.sendEmail(emailParams).promise();
-        return _200()
-    } catch (error) {
-        console.log('error sending email', error)
-        return _400({message: 'unable to send an email'})
-        
-    }
 
+        return _200();
+    } catch (error) {
+        console.log('error sending email', error);
+
+        return _400({ message: 'unable to send the email' });
+    }
 };
 
-const _400 = (body) => {
+const _400 = body => {
     return {
         headers: {
-            'Content-Type': 'application/json',    
+            'Content-Type': 'application/json',
             'Access-Control-Allow-Methods': '*',
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
         },
         statusCode: 400,
         body: JSON.stringify(body),
     };
-
 };
-
-const _200 = (body) => {
+const _200 = () => {
     return {
         headers: {
-            'Content-Type': 'application/json',    
+            'Content-Type': 'application/json',
             'Access-Control-Allow-Methods': '*',
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
         },
         statusCode: 200,
-        body: ''
+        body: '',
     };
-
 };
