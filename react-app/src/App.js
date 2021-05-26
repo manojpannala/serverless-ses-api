b296c21a217c0cf169092dc288dd4bf736138944
manojpannala <manojpannala@outlook.com>
2021-05-26 10:57:33 +0300

    add react-app

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './Code typing-bro.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>My Email application</h1>
+      </header>
+      Email Container
+    </div>
+  );
+}
+
+export default App;
